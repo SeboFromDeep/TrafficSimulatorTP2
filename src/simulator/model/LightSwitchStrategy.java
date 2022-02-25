c9b700bf3774118dc807c494982837feb013767b
SeboFromDeep <49608175+SeboFromDeep@users.noreply.github.com>
2022-02-25 19:51:28 +0100

    Delete LightSwitchStrategy.java

--- src/simulator/model/LightSwitchStrategy.java
+++
@@ -1,9 +0,0 @@
-package simulator.model;
-
-import java.util.List;
-
-public interface LightSwitchStrategy {
-
-	int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime, int currTime);
-
-}
