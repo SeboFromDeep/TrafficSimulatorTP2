235fa5ec73e77711bc8c9d2844b5642bd1441f98
SeboFromDeep <sebashoyomavs@gmail.com>
2022-03-03 16:24:54 +0100

    mcsb pasa tests

--- src/simulator/factories/MostCrowdedStrategyBuilder.java
+++ src/simulator/factories/MostCrowdedStrategyBuilder.java
@@ -3,19 +3,19 @@ package simulator.factories;
 import org.json.JSONObject;
 
 import simulator.model.LightSwitchingStrategy;
-import simulator.model.RoundRobinStrategy;
+import simulator.model.MostCrowdedStrategy;
 
-public class RoundRobinStrategyBuilder extends Builder<LightSwitchingStrategy>{
+public class MostCrowdedStrategyBuilder extends Builder<LightSwitchingStrategy> {
 
-	RoundRobinStrategyBuilder() {
-		super("round_robin_lss");
+	MostCrowdedStrategyBuilder() {
+		super("most_crowded_lss");
 	}
 
 	@Override
 	protected LightSwitchingStrategy createTheInstance(JSONObject data) {
 		int timeSlot = 1;
 		if (data != null && data.has("timeslot")) timeSlot = data.getInt("timeslot");
-		return new RoundRobinStrategy(timeSlot);
+		return new MostCrowdedStrategy(timeSlot);
 	}
 
 }
