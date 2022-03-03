88952165301c655d3acdfda25b5a08baba13a146
SeboFromDeep <sebashoyomavs@gmail.com>
2022-03-03 17:59:30 +0100

    newintercityroadbuilder pasa tests

---
+++ src/simulator/factories/NewInterCityRoadEventBuilder.java
@@ -0,0 +1,30 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+
+import simulator.model.Event;
+import simulator.model.NewCityRoadEvent;
+import simulator.model.Weather;
+
+public class NewInterCityRoadEventBuilder extends Builder<Event> {
+
+	NewInterCityRoadEventBuilder() {
+		super("new_inter_city_road");
+	}
+
+	@Override
+	protected Event createTheInstance(JSONObject data) {
+		int time = data.getInt("time");
+		String id = data.getString("id");
+		String src = data.getString("src");
+		String dest = data.getString("dest");
+		int length = data.getInt("length");
+		int contLimit = data.getInt("co2limit");
+		int maxSpeed = data.getInt("maxspeed");
+		
+		String s = data.getString("weather").toUpperCase();
+		Weather w = Weather.valueOf(s.toUpperCase());
+		return new NewCityRoadEvent(time, id, src, dest, length, contLimit, maxSpeed, w);
+	}
+
+}
