659bfce05d9c6c9fe9e83e430f446d50632a8758
SeboFromDeep <sebashoyomavs@gmail.com>
2022-03-03 18:35:30 +0100

    add de setweathereventbuilder

---
+++ src/simulator/factories/SetWeatherEventBuilder.java
@@ -0,0 +1,41 @@
+package simulator.factories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import simulator.misc.Pair;
+import simulator.model.Event;
+import simulator.model.SetWeatherEvent;
+import simulator.model.Weather;
+
+public class SetWeatherEventBuilder extends Builder<Event> {
+
+	SetWeatherEventBuilder() {
+		super("set_weather");
+	}
+
+	@Override
+	protected Event createTheInstance(JSONObject data) {
+		int time = data.getInt("time");
+		
+		JSONArray roadsJson = data.getJSONArray("info");
+		List<Pair<String, Weather>> roadsList = new ArrayList<>();
+		for (int i = 0; i < roadsJson.length(); i++) {
+			JSONObject r = roadsJson.getJSONObject(i);
+			String id = r.getString("road");
+			Weather w = Weather.valueOf(data.getString("weather").toUpperCase());
+			roadsList.add(new Pair<String, Weather>(id, w));
+		}
+		
+		try {
+			return new SetWeatherEvent(time, roadsList);
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return null;
+	}
+
+}
